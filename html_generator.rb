dffda088b5f1c9d4fe0a92c0eda4c6c08c6ad867
Brian Han <brianhan87usa@gmail.com>
2013-08-31 15:34:44 -0400

    Refactor html_generator.rb and router.rb

--- html_generator.rb
+++ html_generator.rb
@@ -2,38 +2,35 @@ require 'json'
 require 'open-uri'
 
 class HtmlGenerator
-	def index
+	attr_accessor :products 
+	
+	def initialize
 		raw_response = open("http://lcboapi.com/products").read
 
-		#Parse JSON-formatted text into a Ruby Hash
+		# Parse JSON-formatted text into a Ruby Hash
 		parsed_response = JSON.parse(raw_response)
 
-		# #Return the actual result data from the respose, ignoring metadata
-		@products = parsed_response["result"]
-
-		@products.each do |product|
-		 	puts product["name"]
-		end	
+		# Return the actual result data from the response, 
+		# ignoring metadata
+		#@products = parsed_response["result"].first["id"]
+		@products   = parsed_response["result"]
+	
 	end
+
+	def index
+		puts "HtmlGenerator: index"
+		puts @products		
+	end
+
 	def show(product_id)
-		#write the same as the index method but passing a product_id in 
-		raw_response = open("http://lcboapi.com/products").read
-
-		#Parse JSON-formatted text into a Ruby Hash
-		parsed_response = JSON.parse(raw_response)
-
-		# #Return the actual result data from the respose, ignoring metadata
-		products = parsed_response["result"]
-
-		products.each do |product|
-			if product["id"] == product_id
-				puts "Name: #{product["name"]}"
-				puts "Primary Category: #{product["primary_category"]}"
-				puts "Secondary Category: #{product["secondary_category"]}"
-				puts "Origin: #{product["origin"]}"
-				
-
+		@products.each do |product|
+			# puts product.fetch("id").class
+			# puts product_id.class
+			if product.fetch("id") == product_id.to_i
+				product.each do |key, value|
+					puts "#{key} : #{value}"
+				end
 			end
-		end	
+		end
 	end
 end
--- router.rb
+++ router.rb
@@ -3,14 +3,14 @@ require_relative 'html_generator'
 if ARGV.empty?
 	puts "Usage: ruby router.rb [action]"
 else
-	action = ARGV[0]
+	action, product_id = ARGV
 	generator = HtmlGenerator.new
 
 	if action == "index"
 		generator.index
 	elsif action == "show"
-		generator.show(ARGV[1].to_i)
+		generator.show(product_id)
 	else
-		puts "Unkown action #{action}. Use index or show"
+		puts "Unknown action #{action}. Use index or show."
 	end
 end
