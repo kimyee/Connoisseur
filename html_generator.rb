b3f4d0af581dd374a7c5384ad55b48a0ac18b96a
kimyee <kimyee.16@gmail.com>
2013-08-31 21:16:46 -0400

    styling

--- html_generator.rb
+++ html_generator.rb
@@ -2,7 +2,7 @@ require 'json'
 require 'open-uri'
 
 class HtmlGenerator
-	attr_accessor :products 
+	attr_accessor :products, :html_start, :html_end
 	
 	def initialize
 		raw_response = open("http://lcboapi.com/products").read
@@ -14,29 +14,47 @@ class HtmlGenerator
 		# ignoring metadata
 		#@products = parsed_response["result"].first["id"]
 		@products   = parsed_response["result"]
+
+		@html_start = [ 
+			"<!DOCTYPE HTML>\n", 
+			"<html lang='en'>\n", 
+			"<head>\n", "<link rel='stylesheet' type='text/css' href='style.css'>\n",
+			"<link rel='stylesheet' type='text/css' href='reset.css'>\n",
+			"<title>Getting Crunked</title>\n",
+			"</head>\n",
+			"<body>\n"
+		]
+
+		@html_end = [
+		"</div> <!-- close product div -->\n",
+		"</div>  <!-- close container div -->\n",
+		"<div id='footer'>Footer</div> <!-- close footer div -->\n",
+		"</body>\n",
+		"</html>\n"
+		]
+ 	
 	
 	end
-
+	
 	def index
-		# puts "HtmlGenerator: index"
-		# puts @products
+		@html_start.each { |tag| puts tag}
+		puts "<div id='container'>"
+		puts	"<div id='header'>Header</div><!-- close header div -->" 
+		puts	"<div class='products'>"
 
-		@products.each do |product|
-			product.each do |key, value|
-				puts "#{key} : #{value}"
-
-			end 
+		puts "<div class='container2'>"
+		@products.each do |product| 
+			if product.has_key?("image_thumb_url") && product.has_key?("id")
+				puts "<div class='id'>#{product['id']}</div>"
+				puts "<div class='product_img'><img src='#{product["image_thumb_url"]}'></div>"	
+			end
 		end
-			
+		puts "</div>"
+		@html_end.each { |tag| puts tag }
 	end
-
-
-
 
 	def show(product_id)
 		@products.each do |product|
-			# puts product.fetch("id").class
-			# puts product_id.class
 			if product.fetch("id") == product_id.to_i
 				product.each do |key, value|
 					puts "#{key} : #{value}"
